138ee8234f35df05f8d7596ac9999b4a8c5e04e5
andy <andy@molimama.cn>
2017-05-18 11:29:36 +0800

    'v-router-init';

---
+++ src/components/navigator.js
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+import { Link, browserHistory } from 'react-router';
+
+class NavigatorView extends Component{
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		return (
+			<div>
+				<ul>
+					
+				</ul>
+			</div>
+		)
+	}
+}
